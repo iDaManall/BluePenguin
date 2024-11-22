abb83f6b302ade9fcb6eb99ef46f79e377bc3ebd
iDaManall <idamanallgt@gmail.com>
2024-11-21 21:27:17 -0500

    removed protected route, draft home page/navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,8 @@
-import react from "react"
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import Login from "./pages/Auth/Login"
 import Register from "./pages/Auth/Register"
-import Home from "./pages/Home"
+import Home from "./pages/Home/Home"
 import NotFound from "./pages/NotFound"
-import ProtectedRoute from "./components/ProtectedRoute"
 
 // clear refresh and access token when we log out, navigate to login page
 function Logout(){
@@ -21,22 +19,14 @@ function RegisterAndLogout() {
 function App() {
 
   return (
-    
     <BrowserRouter>
-    {/*all of the different routes we want to navigate between*/}
+      {/*all of the different routes we want to navigate between*/}
       <Routes>
-        <Route 
-        path = "/" 
-        element = {
-          // can only access home page if authenticated/logged in
-          <ProtectedRoute>
-            <Home />
-          </ProtectedRoute>
-        }
-        />
+        <Route path = "/" element = {<Home />} />
         <Route path = "/login" element = {<Login />} />
         <Route path = "/logout" element = {<Logout />} />
         <Route path = "/register" element = {<RegisterAndLogout />} />
+        <Route path = "/home" element = {<Home />} />
         <Route path = "*" element = {<NotFound />}></Route>
       </Routes>
     </BrowserRouter>
