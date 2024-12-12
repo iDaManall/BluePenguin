b2fe7e9c6f8392793bae2a55ee2e618949b93c15
addinar <raddina24@gmail.com>
2024-12-12 12:46:51 -0500

    fixed certificate

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -228,6 +228,9 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
+EMAIL_USE_SSL = False
+EMAIL_USE_TLS = True
+
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
